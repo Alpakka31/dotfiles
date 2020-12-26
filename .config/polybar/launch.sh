8837a8feafc9cefaf2b4af4f9d9d1cd954e577c8
Alpakka31 <patukka31@gmail.com>
2020-12-26 13:25:39 +0200

    update polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,14 +9,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar with multi-monitor support
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#        MONITOR=$m polybar --reload topleft-laptop &
-#        MONITOR=$m polybar --reload topright-laptop &
-         MONITOR=$m polybar --reload topleft-desktop &
-         MONITOR=$m polybar --reload topright-desktop &
+        MONITOR=$m polybar --reload mainbar &
     done
 else
-#    polybar --reload topleft-laptop &
-#    polybar --reload topright-laptop &
-     polybar --reload topleft-desktop &
-     polybar --reload topleft-desktop &
+    polybar --reload mainbar &
 fi
